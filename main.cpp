561303461a23f6f22011851611a06d0c063813fa
Ryeel <legendaryfirelfg@gmail.com>
2026-05-26 07:58:06 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#define rele 3
+#define sensor 2
+
+bool irrigar = false;
+void setup()
+{
+  pinMode(rele, OUTPUT);
+  pinMode(sensor, INPUT);
+
+  digitalWrite( rele, HIGH);
+}
+
+void loop()
+{
+  irrigar = digitalRead(sensor);
+
+  if(irrigar)
+  {
+    digitalWhite(rele, LOW);
+  }
+  else
+  {
+    digitalWrite(rele, HIGH);
+  }
+
+delay(500);
+}
